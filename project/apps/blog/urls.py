a76d240adba6d72720fbfb4e0c54a11314f8ed8b
dean <dean_antonic@hotmail.com>
2016-02-26 11:25:45 -0800

    deleted some redundancies

--- project/apps/blog/urls.py
+++ project/apps/blog/urls.py
@@ -1,15 +1,8 @@
-from __future__ import unicode_literals
-from django.conf import settings
-from django.conf.urls import include, url
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.contrib.sites.models import Site
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib.sitemaps.views import sitemap
-from django.utils.translation import ugettext_lazy as _
 
-from cms.sitemaps import CMSSitemap
-from solid_i18n.urls import solid_i18n_patterns as i18n_patterns
+from django.conf.urls import include, url
+
+
+
 
 urlpatterns = [
     url(r'^$', 'blog.views.index'),	
